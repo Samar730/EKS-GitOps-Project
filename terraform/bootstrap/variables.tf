7c22e08ff9a21a8f02419e6bf4008076932375ab
Samar730 <samar.akbari1@gmail.com>
2026-05-10 21:25:40 +0100

    Feat: Created bootstrap resources

---
+++ terraform/bootstrap/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+    type = string
+    default = "eu-west-2"
+}
+
+variable "project_name" {
+    type = string
+    default = "eks-memos"
+}
+
+variable "github_repo" {
+    type = string
+    default = "Samar730/EKS-GitOps-Project"
+}
