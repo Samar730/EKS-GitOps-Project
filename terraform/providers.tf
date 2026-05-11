2e6b70ef221aa326339772b8c2dc9addbe597e3d
Samar730 <samar.akbari1@gmail.com>
2026-05-11 17:26:20 +0100

    feat: add root terraform provider and variables

---
+++ terraform/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket = "eks-memos-tfstate"
+    key    = "terraform.tfstate"
+    region = "eu-west-2"
+    use_lockfile = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.44.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = var.aws_region
+}
